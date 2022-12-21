3c9f3fd4fb8099bb7e77e38e8dc8e7d843fb8215
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2022-12-21 21:23:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Sale order line menu with Prepress Proof field
+----------------------------------------------------
+This module add the Prepress Proof field to sale order line menu.
---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Sale order line menu with Prepress Proof field',
+    'version': '1.0.1',
+    'author':'Soft-integration',
+    'category': 'Sale/Prepress',
+    'description': "",
+    'depends': [
+        'sale_order_line_menu','sale_prepress_management'
+    ],
+    'data': [
+        'views/sale_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
